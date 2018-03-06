79de5a33e92a57724891bc16afeeb38ad8d467de
MAJIN123 <806379655@qq.com>
2018-03-06 15:46:17 +0800

    2011 part

--- 2011/main.cpp
+++ 2011/main.cpp
@@ -14,8 +14,39 @@
 //最后输出cip1和cip2以及第一段完整的英文句子。
 
 //#include <iostream>
-#include <stdio.h>
-int main() {
 
+#include <stdio.h>
+#include <math.h>
+int Is_prime(int a){
+    if(a<2)
+        return 0;
+
+    for(int i=2;i<sqrt(a*1.0);++i) {
+        if (a % i == 0)
+            return 0;
+    }
+    return 1;
+}
+void Question_1(){
+//(1)该数是素数
+//(2)十位数和个位数组成的数是素数，百位数和个位数组成的数是素数
+//(3)千位数和百位数组成的数是素数，千位数和十位数组成的数是素数
+    for(int i=1000;i<10000;++i){
+        int num[4];
+        num[3]=i/1000;//千
+        num[2]=(i/100)%10;//百
+        num[0]=i%10;//个
+        num[1]=(i/10)%10;//十
+        //printf("%d%d%d%d%d\n",num[3],num[2],num[1],num[0],i);
+        int x=num[2]*10+num[0];//百 个
+        int y=num[3]*10+num[1];
+        //printf("%d  %d  %d\n",x,y,i);
+        if(Is_prime(i)&&Is_prime(i%100)&&Is_prime(x)&&Is_prime(i/100)&&Is_prime(y)){
+            printf("%d\n",i);
+        }
+    }
+}
+int main() {
+    Question_1();
     return 0;
 }
